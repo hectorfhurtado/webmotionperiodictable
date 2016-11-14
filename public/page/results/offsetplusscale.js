7ed8db55121b2809bd2dda2e7e5cd7ff4906069b
hectorfhurtado <hurtado.fernando@gmail.com>
2016-11-13 19:52:35 -0500

    Added offset cell

--- public/page/results/offsetplusscale.js
+++ public/page/results/offsetplusscale.js
@@ -14,14 +14,23 @@
 		_appendTo( $container );
 	}
 
-	// TODO: continue with green hexagon animation
-
+	/**
+	 * We append an hexagon to every container to apply them its animation
+	 * @param	{Object}	$container
+	 * @private
+	 */
 	function _appendTo( $container )
 	{
 		const $offsetTemplate  = $( '#offsetplusscale_template' );
 		const $hexagonTemplate = $( '#HexagonSvg' );
 
 		$offsetTemplate.content.querySelector( '.OffsetPlusScale-greenHexagon' )
+			.appendChild( document.importNode( $hexagonTemplate.content, true ));
+
+		$offsetTemplate.content.querySelector( '.OffsetPlusScale-innerHexagon' )
+			.appendChild( document.importNode( $hexagonTemplate.content, true ));
+
+		$offsetTemplate.content.querySelector( '.OffsetPlusScale-outerHexagon' )
 			.appendChild( document.importNode( $hexagonTemplate.content, true ));
 
 		$container.appendChild( document.importNode( $offsetTemplate.content, true ));
