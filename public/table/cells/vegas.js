694726c559470982101285bf9b58b99ee8b21745
hectorfhurtado <hurtado.fernando@gmail.com>
2017-05-01 06:59:03 -0500

    Finished Vegas icon

--- public/table/cells/vegas.js
+++ public/table/cells/vegas.js
@@ -15,16 +15,57 @@
 			this.shadowRoot.innerHTML = `
 				<style>
 					.Cell_canvas {
-						width: 100%;
-						height: 100%;
+						width:    100%;
+						height:   100%;
+						overflow: hidden;
+						display:  flex;
+					}
+
+					.lines-container {
+						position:   relative;
+						margin:     auto;
+						width:      80%;
+						height:     50%;
+						margin-top: 55%;
+						overflow:   hidden;
+					}
+
+					.top {
+						position: absolute;
+						top:      0;
+						left:     0;
+						right:    0;
+					}
+
+					.right {
+						--right-distance: calc(100% - 2px);
+
+						transform: translateX(var(--right-distance)) translateY(-5px) rotate(90deg);
+						transform-origin: left center;
 						overflow: hidden;
 					}
 
-					.right { transform: rotate(90deg) }
+					.bottom {
+						--bottom-distance: calc(1510%);
+
+						transform: translateX(100%) translateY(var(--bottom-distance)) rotate(180deg);
+						transform-origin: left center;
+						overflow: hidden;
+					}
+
+					.left {
+						transform: translateX(1px) translateY(1261%) rotate(270deg);
+						transform-origin: left center;
+						overflow: hidden;
+					}
 				</style>
 				<div class="Cell_canvas">
-					<vegas-line></vegas-line>
-					<vegas-line class="right"></vegas-line>
+					<div class="lines-container">
+						<vegas-line class="top"></vegas-line>
+						<vegas-line class="right"></vegas-line>
+						<vegas-line class="bottom"></vegas-line>
+						<vegas-line class="left"></vegas-line>
+					</div>
 				</div>
 				<cell-title>Vegas</cell-title>
 			`;
